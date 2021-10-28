510fb6e4fe5a628939e2b2e2178737025119f38b
unknown <hasan.rajib1996@gmail.com>
2021-10-28 23:38:19 +0600

    added hello api

--- index.js
+++ index.js
@@ -70,6 +70,10 @@ app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('hello updated here');
+})
+
 
 app.listen(port, () => {
     console.log('Running Genius Server on port ', port);
